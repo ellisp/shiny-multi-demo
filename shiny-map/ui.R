bd199d69d1a8407b5ac6b7bd54d3c76e5ac91684
Peter Ellis <petere@spc.int>
2023-12-06 11:38:29 +1200

    center picture and move selector to right

--- shiny-map/ui.R
+++ shiny-map/ui.R
@@ -20,17 +20,19 @@ shinyUI(fluidPage(
     column(5,
       h3("Proportion of adults with year 10 or lower education"),
       leafletOutput("map"),
-      
-      htmlOutput("image_text"),
-     
-      selectInput("region", "Choose a region", choices = all_sa4s, selected = all_sa4s[1])
+      hr(),
+      htmlOutput("image_text")
+   
       ),
     
     column(7,
            htmlOutput("chart_title"),
-           ggvisOutput("barchart")
+           ggvisOutput("barchart"),
+           selectInput("region", "Choose a region", choices = all_sa4s, selected = all_sa4s[1])
+           
     )
   ),
+  hr(),
   fluidRow(
     HTML("<p>A minimal demo of building a Shiny app with a map and a chart.</p>")
   )
